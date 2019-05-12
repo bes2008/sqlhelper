fe4da773676ec68eff7e2247e34292947eda0beb
fs1194361820@163.com <fs1194361820@163.com>
2019-05-12 18:32:30 +0800

    * support swagger ui

---
+++ sqlhelper-examples/src/main/java/com/fjn/helper/examples/mybatis/spring/boot/config/Swagger2Config.java
@@ -0,0 +1,39 @@
+package com.fjn.helper.examples.mybatis.spring.boot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fjn.helper.examples.mybatis.spring.boot"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("更多请关注http://www.baidu.com")
+                .termsOfServiceUrl("http://www.baidu.com")
+                .contact("jinuo.fang")
+                .version("1.0")
+                .build();
+
+    }
+}
