168cbe72533c27c3b34a6bd30374b3c6e811338a
fjn <fs1194361820@163.com>
2019-05-14 14:14:40 +0800

    * supports migrate your application from mybatis-pagehelper

---
+++ sqlhelper-mybatis-over-pagehelper/src/main/java/com/github/pagehelper/PageHelper.java
@@ -0,0 +1,226 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 abel533@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.pagehelper;
+
+import com.fjn.helper.sql.dialect.pagination.PagingRequest;
+import com.fjn.helper.sql.dialect.pagination.PagingRequestContextHolder;
+import com.github.pagehelper.util.PageObjectUtil;
+
+import java.util.Properties;
+
+public class PageHelper{
+    private static final ThreadLocal<Page> LOCAL_PAGE = new ThreadLocal<Page>();
+    private static boolean DEFAULT_COUNT = true;
+
+    /**
+     * 设置 Page 参数
+     *
+     * @param page
+     */
+    protected static void setLocalPage(Page page) {
+        LOCAL_PAGE.set(page);
+        PagingRequest pagingRequest = new PagingRequestAdapter().setPage(page);
+        PagingRequestContextHolder.getContext().setPagingRequest(pagingRequest);
+    }
+
+    public static class PagingRequestAdapter extends PagingRequest{
+        private Page page;
+
+        PagingRequestAdapter(){
+
+        }
+        PagingRequestAdapter setPage(Page page){
+            this.page = page;
+            this.setCount(page.isCount());
+            this.setOrderBy(page.getOrderBy());
+            this.setPageNo(page.getPageNum());
+            this.setPageSize(page.getPageSize());
+            return this;
+        }
+
+        @Override
+        public void clear() {
+            page.close();
+            super.clear();
+        }
+    }
+
+    /**
+     * 获取 Page 参数
+     *
+     * @return
+     */
+    public static <T> Page<T> getLocalPage() {
+        return LOCAL_PAGE.get();
+    }
+
+    /**
+     * 移除本地变量
+     */
+    public static void clearPage() {
+        LOCAL_PAGE.remove();
+    }
+
+    /**
+     * 获取任意查询方法的count总数
+     *
+     * @param select
+     * @return
+     */
+    public static long count(ISelect select) {
+        Page<?> page = startPage(1, -1, true);
+        select.doSelect();
+        return page.getTotal();
+    }
+
+    /**
+     * 开始分页
+     *
+     * @param params
+     */
+    public static <E> Page<E> startPage(Object params) {
+        Page<E> page = PageObjectUtil.getPageFromObject(params, true);
+        //当已经执行过orderBy的时候
+        Page<E> oldPage = getLocalPage();
+        if (oldPage != null && oldPage.isOrderByOnly()) {
+            page.setOrderBy(oldPage.getOrderBy());
+        }
+        setLocalPage(page);
+        return page;
+    }
+
+    /**
+     * 开始分页
+     *
+     * @param pageNum  页码
+     * @param pageSize 每页显示数量
+     */
+    public static <E> Page<E> startPage(int pageNum, int pageSize) {
+        return startPage(pageNum, pageSize, DEFAULT_COUNT);
+    }
+
+    /**
+     * 开始分页
+     *
+     * @param pageNum  页码
+     * @param pageSize 每页显示数量
+     * @param count    是否进行count查询
+     */
+    public static <E> Page<E> startPage(int pageNum, int pageSize, boolean count) {
+        return startPage(pageNum, pageSize, count, null, null);
+    }
+
+    /**
+     * 开始分页
+     *
+     * @param pageNum  页码
+     * @param pageSize 每页显示数量
+     * @param orderBy  排序
+     */
+    public static <E> Page<E> startPage(int pageNum, int pageSize, String orderBy) {
+        Page<E> page = startPage(pageNum, pageSize);
+        page.setOrderBy(orderBy);
+        return page;
+    }
+
+    /**
+     * 开始分页
+     *
+     * @param pageNum      页码
+     * @param pageSize     每页显示数量
+     * @param count        是否进行count查询
+     * @param reasonable   分页合理化,null时用默认配置
+     * @param pageSizeZero true且pageSize=0时返回全部结果，false时分页,null时用默认配置
+     */
+    public static <E> Page<E> startPage(int pageNum, int pageSize, boolean count, Boolean reasonable, Boolean pageSizeZero) {
+        Page<E> page = new Page<E>(pageNum, pageSize, count);
+        page.setReasonable(reasonable);
+        page.setPageSizeZero(pageSizeZero);
+        //当已经执行过orderBy的时候
+        Page<E> oldPage = getLocalPage();
+        if (oldPage != null && oldPage.isOrderByOnly()) {
+            page.setOrderBy(oldPage.getOrderBy());
+        }
+        setLocalPage(page);
+        return page;
+    }
+
+    /**
+     * 开始分页
+     *
+     * @param offset 起始位置，偏移位置
+     * @param limit  每页显示数量
+     */
+    public static <E> Page<E> offsetPage(int offset, int limit) {
+        return offsetPage(offset, limit, DEFAULT_COUNT);
+    }
+
+    /**
+     * 开始分页
+     *
+     * @param offset 起始位置，偏移位置
+     * @param limit  每页显示数量
+     * @param count  是否进行count查询
+     */
+    public static <E> Page<E> offsetPage(int offset, int limit, boolean count) {
+        Page<E> page = new Page<E>(new int[]{offset, limit}, count);
+        //当已经执行过orderBy的时候
+        Page<E> oldPage = getLocalPage();
+        if (oldPage != null && oldPage.isOrderByOnly()) {
+            page.setOrderBy(oldPage.getOrderBy());
+        }
+        setLocalPage(page);
+        return page;
+    }
+
+    /**
+     * 排序
+     *
+     * @param orderBy
+     */
+    public static void orderBy(String orderBy) {
+        Page<?> page = getLocalPage();
+        if (page != null) {
+            page.setOrderBy(orderBy);
+        } else {
+            page = new Page();
+            page.setOrderBy(orderBy);
+            page.setOrderByOnly(true);
+            setLocalPage(page);
+        }
+    }
+
+    /**
+     * 设置参数
+     *
+     * @param properties 插件属性
+     */
+    protected static void setStaticProperties(Properties properties){
+        //defaultCount，这是一个全局生效的参数，多数据源时也是统一的行为
+        if(properties != null){
+            DEFAULT_COUNT = Boolean.valueOf(properties.getProperty("defaultCount", "true"));
+        }
+    }
+}
---
+++ sqlhelper-mybatis-over-pagehelper/src/main/java/com/github/pagehelper/util/MetaObjectUtil.java
@@ -0,0 +1,63 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 abel533@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.pagehelper.util;
+
+import com.github.pagehelper.PageException;
+import org.apache.ibatis.reflection.MetaObject;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author liuzh
+ */
+public class MetaObjectUtil {
+    public static Method method;
+
+    static {
+        try {
+            // 高版本中的 MetaObject.forObject 有 4 个参数，低版本是 1 个
+            // 下面这个 MetaObjectWithReflectCache 带反射的缓存信息
+            Class<?> metaClass = Class.forName("com.github.pagehelper.util.MetaObjectWithReflectCache");
+            method = metaClass.getDeclaredMethod("forObject", Object.class);
+        } catch (Throwable e1) {
+            try {
+                Class<?> metaClass = Class.forName("org.apache.ibatis.reflection.MetaObject");
+                method = metaClass.getDeclaredMethod("forObject", Object.class);
+            } catch (Exception e2) {
+                throw new PageException(e2);
+            }
+        }
+
+    }
+
+    public static MetaObject forObject(Object object) {
+        try {
+            return (MetaObject) method.invoke(null, object);
+        } catch (Exception e) {
+            throw new PageException(e);
+        }
+    }
+
+}
