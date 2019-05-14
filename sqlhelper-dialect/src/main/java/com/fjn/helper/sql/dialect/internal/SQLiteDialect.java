e60a053f91b3fcef6055a435b88a6080e37b1217
fjn <fs1194361820@163.com>
2019-05-14 21:01:19 +0800

    * refactor sqlite dialect

--- sqlhelper-dialect/src/main/java/com/fjn/helper/sql/dialect/internal/SQLiteDialect.java
+++ sqlhelper-dialect/src/main/java/com/fjn/helper/sql/dialect/internal/SQLiteDialect.java
@@ -18,17 +18,12 @@ package com.fjn.helper.sql.dialect.internal;
 import com.fjn.helper.sql.dialect.RowSelection;
 import com.fjn.helper.sql.dialect.internal.limit.AbstractLimitHandler;
 import com.fjn.helper.sql.dialect.internal.limit.LimitHelper;
+import com.fjn.helper.sql.dialect.internal.limit.LimitOffsetLimitHandler;
 
 public class SQLiteDialect extends AbstractDialect {
     public SQLiteDialect() {
         super();
-        setLimitHandler(new AbstractLimitHandler() {
-            @Override
-            public String processSql(String sql, RowSelection selection) {
-                final boolean hasOffset = LimitHelper.hasFirstRow(selection);
-                return sql + (hasOffset ? " limit ? offset ?" : " offset ?");
-            }
-        });
+        setLimitHandler(new LimitOffsetLimitHandler());
     }
 
     @Override
