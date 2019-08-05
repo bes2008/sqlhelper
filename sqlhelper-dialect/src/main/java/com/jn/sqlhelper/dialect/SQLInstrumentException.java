d8993a9cc1b10b483fcd7bb32e537d0f87b35f0a
fjn <fs1194361820@163.com>
2019-08-05 15:53:20 +0800

    * supports order by instrumentor

---
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/SQLInstrumentException.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the LGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jn.sqlhelper.dialect;
+
+public class SQLInstrumentException extends RuntimeException {
+    public SQLInstrumentException(String message) {
+        super(message);
+    }
+
+    public SQLInstrumentException(Throwable ex) {
+        super(ex);
+    }
+}
