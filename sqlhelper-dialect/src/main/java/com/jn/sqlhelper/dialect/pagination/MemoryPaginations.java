2960aef1cb12e67aad52f7244295c68aa00fa38c
fjn <fs1194361820@163.com>
2019-08-28 17:27:44 +0800

    * supports memory pagination

--- sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/pagination/MemoryPaginations.java
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/pagination/MemoryPaginations.java
@@ -1,5 +1,7 @@
 package com.jn.sqlhelper.dialect.pagination;
 
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.util.Emptys;
 import com.jn.langx.util.Preconditions;
 import com.jn.langx.util.collection.Collects;
 import com.jn.langx.util.collection.Pipeline;
@@ -22,14 +24,15 @@ import java.util.*;
  * @author jinuo.fang
  */
 @SuppressWarnings({"unchecked"})
-public class PseudoPaginations {
-    private static final Logger logger = LoggerFactory.getLogger(PseudoPaginations.class);
+public class MemoryPaginations {
+    private static final Logger logger = LoggerFactory.getLogger(MemoryPaginations.class);
 
-    public static <C, E> List<E> pseudoPaging(Collection<E> list, PagingRequest<C, E> pagingRequest, Predicate<E> filter) {
+    public static <C, E> List<E> pseudoPaging(Collection<E> list, PagingRequest<C, E> pagingRequest, Predicate<E>... filters) {
         Preconditions.checkNotNull(list);
         Preconditions.checkNotNull(pagingRequest);
 
         // step 1: do filter
+        Predicate<E> filter = Emptys.isNotEmpty(filters) ? allPredicate(filters) : null;
         List<E> filtered = Collects.asList(filter != null ? Collects.filter(list, filter) : list);
 
         // step 2: build paging result
@@ -72,8 +75,24 @@ public class PseudoPaginations {
         return rs;
     }
 
+    private static <E> Predicate<E> allPredicate(@NonNull Predicate<E>... predicates) {
+        Preconditions.checkTrue(Emptys.isNotEmpty(predicates));
+        final Pipeline<Predicate<E>> pipeline = Pipeline.<Predicate<E>>of(predicates);
+        return new Predicate<E>() {
+            @Override
+            public boolean test(final E value) {
+                return pipeline.allMatch(new Predicate<Predicate<E>>() {
+                    @Override
+                    public boolean test(Predicate<E> filter) {
+                        return filter.test(value);
+                    }
+                });
+            }
+        };
+    }
 
-    public static <E> List<E> doSort(Collection<E> collection, OrderBy orderBy) {
+
+    private static <E> List<E> doSort(Collection<E> collection, OrderBy orderBy) {
         if (collection.isEmpty() || !orderBy.isValid()) {
             return Collects.asList(collection);
         }
@@ -86,9 +105,14 @@ public class PseudoPaginations {
             String fieldName = orderByItem.getExpression();
             Field field = Reflects.getDeclaredField(modelClass, fieldName);
             if (field != null) {
-                Class fieldClass = field.getType();
-                if (Comparable.class.isAssignableFrom(fieldClass)) {
-                    Comparator comparator = new ComparableComparator();
+                Comparator comparator = orderByItem.getComparator();
+                if (comparator == null) {
+                    Class fieldClass = field.getType();
+                    if (Comparable.class.isAssignableFrom(fieldClass)) {
+                        comparator = new ComparableComparator();
+                    }
+                }
+                if (comparator != null) {
                     comparator = new FieldComparator(field, comparator);
                     if (orderByItem.getType() == OrderByType.DESC) {
                         comparator = new ReverseComparator(comparator);
@@ -99,6 +123,7 @@ public class PseudoPaginations {
             } else {
                 logger.warn("can't find a field [{}] in class [{}]", fieldName, Reflects.getFQNClassName(modelClass));
             }
+
         }
         if (i > 0) {
             Set<E> rs = new TreeSet<E>(parallelingComparator);
