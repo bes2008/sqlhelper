00d5a5c5d64dbc5c966586ee16bb11d003b6553c
fjn <fs1194361820@163.com>
2019-09-03 21:00:18 +0800

    * avoid NPE

--- sqlhelper-springjdbc/src/main/java/com/jn/sqlhelper/springjdbc/PaginationPreparedStatementSetter.java
+++ sqlhelper-springjdbc/src/main/java/com/jn/sqlhelper/springjdbc/PaginationPreparedStatementSetter.java
@@ -9,19 +9,23 @@ import java.sql.SQLException;
 
 public class PaginationPreparedStatementSetter implements PrepareParameterSetter {
     private PreparedStatementSetter delegate;
-    public PaginationPreparedStatementSetter(PreparedStatementSetter setter){
+
+    public PaginationPreparedStatementSetter(PreparedStatementSetter setter) {
         delegate = setter;
     }
+
     @Override
     public int setParameters(PreparedStatement statement, QueryParameters queryParameters, int startIndex) throws SQLException {
-        if (delegate!=null && (statement instanceof PaginationPreparedStatement)) {
-            PaginationPreparedStatement pps = (PaginationPreparedStatement) statement;
-            pps.setIndexOffset(startIndex >= 1 ? (startIndex - 1) : -1);
+        if (delegate != null) {
+            if ((statement instanceof PaginationPreparedStatement)) {
+                PaginationPreparedStatement pps = (PaginationPreparedStatement) statement;
+                pps.setIndexOffset(startIndex >= 1 ? (startIndex - 1) : -1);
+                delegate.setValues(statement);
+                pps.setIndexOffset(-1);
+                return pps.getSetParameterIndexes().size();
+            }
             delegate.setValues(statement);
-            pps.setIndexOffset(-1);
-            return pps.getSetParameterIndexes().size();
         }
-        delegate.setValues(statement);
         return 0;
     }
 }
