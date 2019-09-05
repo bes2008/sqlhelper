1564ee70b9126c4fb96bb72d04ce8f5348751de8
fjn <fs1194361820@163.com>
2019-09-05 13:23:52 +0800

    * add some utilities methods

--- sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/SQLs.java
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/SQLs.java
@@ -2,9 +2,11 @@ package com.jn.sqlhelper.dialect;
 
 import com.jn.langx.util.Strings;
 
+import java.util.Locale;
 import java.util.StringTokenizer;
 
 public class SQLs {
+    //DML
     public static boolean isSelectStatement(String sql) {
         String sql0 = sql.trim();
         // with xx as ( select x ...
@@ -23,4 +25,41 @@ public class SQLs {
             return Strings.startsWithIgnoreCase(sql0, "select");
         }
     }
+
+    public static boolean isUpdateStatement(String sql){
+        sql = sql.trim();
+        return sql.toLowerCase(Locale.ROOT).replaceAll("\\s"," ").matches("update \\w+(\\.\\w)* set");
+    }
+
+    public static boolean isDeleteStatement(String sql){
+        sql = sql.trim();
+        return sql.toLowerCase(Locale.ROOT).replaceAll("\\s"," ").startsWith("delete from");
+    }
+
+    public static boolean isInsertStatement(String sql){
+        sql = sql.trim();
+        return sql.toLowerCase(Locale.ROOT).replaceAll("\\s"," ").startsWith("insert into");
+    }
+
+
+    // DDL
+    public static boolean isCreateStatement(String sql){
+        sql = sql.trim();
+        return sql.toLowerCase(Locale.ROOT).replaceAll("\\s"," ").startsWith("create table");
+    }
+
+    public static boolean isDropTableStatement(String sql){
+        sql = sql.trim();
+        return sql.toLowerCase(Locale.ROOT).replaceAll("\\s"," ").startsWith("drop table");
+    }
+
+    public static boolean isQuote(String tok) {
+        return "\"".equals(tok) ||
+                "`".equals(tok) ||
+                "]".equals(tok) ||
+                "[".equals(tok) ||
+                "'".equals(tok);
+    }
+
+
 }
