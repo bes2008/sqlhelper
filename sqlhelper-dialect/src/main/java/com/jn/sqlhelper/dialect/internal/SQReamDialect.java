777f20622fd7d603e953ead1400d099a28bb2244
fangjinuo <fs1194361820@163.com>
2019-09-25 23:32:46 +0800

    * fix sqream dialect

--- sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/internal/SQReamDialect.java
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/internal/SQReamDialect.java
@@ -14,12 +14,15 @@
 
 package com.jn.sqlhelper.dialect.internal;
 
-import com.jn.sqlhelper.dialect.internal.limit.LimitOnlyLimitHandler;
+import com.jn.sqlhelper.dialect.internal.limit.TopLimitHandler;
 
+/**
+ * http://docs.sqream.com/latest/manual/Content/SQL_Reference_Guide/19_2.4_Queries.htm?tocpath=SQream%20DB%20%20SQL%20Reference%20Guide%7CQueries%7C_____0#Queries_..274
+ */
 public class SQReamDialect extends AbstractDialect {
     public SQReamDialect() {
         super();
-        setLimitHandler(new LimitOnlyLimitHandler());
+        setLimitHandler(new TopLimitHandler());
     }
 
     @Override
