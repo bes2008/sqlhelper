788a6f07231d923c5535a00429b52d7d12dc1d31
fjn <fs1194361820@163.com>
2019-09-27 20:10:27 +0800

    * add some test cases for jdbc driver

--- sqlhelper-examples/src/test/java/com/jn/sqlhelper/examples/test/ddlmodel/GetDatabaseInfoTests.java
+++ sqlhelper-examples/src/test/java/com/jn/sqlhelper/examples/test/ddlmodel/GetDatabaseInfoTests.java
@@ -1,5 +1,7 @@
 package com.jn.sqlhelper.examples.test.ddlmodel;
 
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.annotation.Nullable;
 import com.jn.langx.util.collection.Collects;
 import com.jn.langx.util.io.IOs;
 import com.jn.sqlhelper.common.connection.ConnectionConfiguration;
@@ -10,6 +12,7 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class GetDatabaseInfoTests {
@@ -27,10 +30,18 @@ public class GetDatabaseInfoTests {
         Connection connection = connectionFactory.getConnection();
 
         DatabaseMetaData dbMetaData = connection.getMetaData();
-        showCatalogs(dbMetaData);
+        // showCatalogs(dbMetaData);
+        // showSchemas(dbMetaData);
+        showTables(dbMetaData);
     }
 
-    private void showCatalogs(DatabaseMetaData dbMetaData) throws Exception {
+    /**
+     * catalog.schema.table
+     *
+     * @param dbMetaData
+     * @throws SQLException
+     */
+    private void showCatalogs(DatabaseMetaData dbMetaData) throws SQLException {
         ResultSet catalogRs = dbMetaData.getCatalogs();
         List<String> catalogs = Collects.emptyArrayList();
         while (catalogRs.next()) {
@@ -40,8 +51,64 @@ public class GetDatabaseInfoTests {
 
         String catalogSeparator = dbMetaData.getCatalogSeparator();
         String catalogTerm = dbMetaData.getCatalogTerm();
-        System.out.println("catalogSeparator: " + catalogSeparator);
-        System.out.println("catalogTerm: " + catalogTerm);
+        boolean catalogAtStart = dbMetaData.isCatalogAtStart();
+        int maxCatalogLength = dbMetaData.getMaxCatalogNameLength();
+
+        System.out.println("maxCatalogNameLength: " + maxCatalogLength);
+        System.out.println("catalogSeparator: " + catalogSeparator); // .
+        System.out.println("catalogTerm: " + catalogTerm); //catalog
         System.out.println("catalogs: " + catalogs.toString());
+        System.out.println("catalog at start ? :" + catalogAtStart);
+    }
+
+    private void showSchemas(DatabaseMetaData dbMetaData) throws SQLException {
+
+        ResultSet schemasRs = dbMetaData.getSchemas();
+        while (schemasRs.next()) {
+            String schema = schemasRs.getString("TABLE_SCHEM");
+            String catalog = schemasRs.getString("TABLE_CATALOG");
+            System.out.println(catalog + " " + schema);
+        }
+
+        String schemaTerm = dbMetaData.getSchemaTerm();
+        System.out.println("schema term: " + schemaTerm);
+
+        int maxSchemaNameLength = dbMetaData.getMaxSchemaNameLength();
+        System.out.println("maxSchemaNameLength: " + maxSchemaNameLength);
+    }
+
+    private void showTables(DatabaseMetaData dbMetaData) throws SQLException {
+        int maxTableNameLength = dbMetaData.getMaxTableNameLength();
+        System.out.println("maxTableNameLength: " + maxTableNameLength);
+
+        ResultSet tablesRs = dbMetaData.getTables("TEST","PUBLIC", null, null);
+        while (tablesRs.next()){
+            @Nullable
+            String catalog = tablesRs.getString("TABLE_CAT");
+            @Nullable
+            String schema = tablesRs.getString("TABLE_SCHEM");
+
+            @NonNull
+            String tableName = tablesRs.getString("TABLE_NAME");
+            @NonNull
+            String tableType = tablesRs.getString("TABLE_TYPE");
+
+            @NonNull
+            String comment = tablesRs.getString("REMARKS");
+
+            @Nullable
+            String typeCatalog = tablesRs.getString("TYPE_CAT");
+            @Nullable
+            String typeSchema = tablesRs.getString("TYPE_SCHEM");
+            @Nullable
+            String typeName = tablesRs.getString("TYPE_NAME");
+            @Nullable
+            String SELF_REFERENCING_COL_NAME = tablesRs.getString("SELF_REFERENCING_COL_NAME");
+            @Nullable
+            String REF_GENERATION = tablesRs.getString("SELF_REFERENCING_COL_NAME");
+
+            System.out.println(catalog);
+        }
+
     }
 }
