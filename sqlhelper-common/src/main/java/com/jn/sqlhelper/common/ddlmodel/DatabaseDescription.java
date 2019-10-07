f93021aa2ebd8997a5d89003f55b0de1a9dc070a
fangjinuo <fs1194361820@163.com>
2019-10-07 08:12:26 +0800

    * Supports more database attributes

--- sqlhelper-common/src/main/java/com/jn/sqlhelper/common/ddlmodel/DatabaseDescription.java
+++ sqlhelper-common/src/main/java/com/jn/sqlhelper/common/ddlmodel/DatabaseDescription.java
@@ -31,7 +31,135 @@ public class DatabaseDescription {
         }, null);
     }
 
+    /**
+     * Retrieves whether the current user can call all the procedures
+     * returned by the method <code>getProcedures</code>.
+     *
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     */
     public boolean allProceduresAreCallable() {
         return allProceduresAreCallable;
+    }
+
+    private boolean supportsMixedCaseIdentifiers = false;
+
+    private void parseSupportsMixedCaseIdentifiers() {
+        this.supportsMixedCaseIdentifiers = Throwables.ignoreThrowable(logger, false, new ThrowableFunction<Object, Boolean>() {
+            @Override
+            public Boolean doFun(Object o) throws Throwable {
+                return dbMetaData.supportsMixedCaseIdentifiers();
+            }
+        }, null);
+    }
+
+    /**
+     * Retrieves whether this database treats mixed case unquoted SQL identifiers as
+     * case sensitive and as a result stores them in mixed case.
+     *
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     */
+    public boolean supportsMixedCaseIdentifiers() {
+        return supportsMixedCaseIdentifiers;
+    }
+
+    private boolean supportsMixedCaseQuotedIdentifiers = false;
+
+    private void parseSupportsMixedCaseQuotedIdentifiers() {
+        this.supportsMixedCaseQuotedIdentifiers = Throwables.ignoreThrowable(logger, false, new ThrowableFunction<Object, Boolean>() {
+            @Override
+            public Boolean doFun(Object o) throws Throwable {
+                return dbMetaData.supportsMixedCaseQuotedIdentifiers();
+            }
+        }, null);
+    }
+
+    /**
+     * Retrieves whether this database treats mixed case quoted SQL identifiers as
+     * case sensitive and as a result stores them in mixed case.
+     *
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     */
+    public boolean supportsMixedCaseQuotedIdentifiers() {
+        return supportsMixedCaseQuotedIdentifiers;
+    }
+
+    private boolean supportsSchemasInTableDefinitions;
+
+    private void parseSupportsSchemasInTableDefinitions() {
+        this.supportsSchemasInTableDefinitions = Throwables.ignoreThrowable(logger, false, new ThrowableFunction<Object, Boolean>() {
+            @Override
+            public Boolean doFun(Object o) throws Throwable {
+                return dbMetaData.supportsSchemasInTableDefinitions();
+            }
+        }, null);
+    }
+
+    /**
+     * Retrieves whether a schema name can be used in a table definition statement.
+     *
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     */
+    public boolean supportsSchemasInTableDefinitions() {
+        return supportsSchemasInTableDefinitions;
+    }
+
+    private boolean supportsSchemasInIndexDefinitions;
+
+    private void parseSupportsSchemasInIndexDefinitions() {
+        this.supportsSchemasInIndexDefinitions = Throwables.ignoreThrowable(logger, false, new ThrowableFunction<Object, Boolean>() {
+            @Override
+            public Boolean doFun(Object o) throws Throwable {
+                return dbMetaData.supportsSchemasInIndexDefinitions();
+            }
+        }, null);
+    }
+
+    /**
+     * Retrieves whether a schema name can be used in an index definition statement.
+     *
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     */
+    public boolean supportsSchemasInIndexDefinitions() {
+        return supportsSchemasInIndexDefinitions;
+    }
+
+    private boolean supportsCatalogsInTableDefinitions;
+
+    private void parseSupportsCatalogsInTableDefinitions() {
+        this.supportsCatalogsInTableDefinitions = Throwables.ignoreThrowable(logger, false, new ThrowableFunction<Object, Boolean>() {
+            @Override
+            public Boolean doFun(Object o) throws Throwable {
+                return dbMetaData.supportsCatalogsInTableDefinitions();
+            }
+        }, null);
+    }
+
+    /**
+     * Retrieves whether a catalog name can be used in a table definition statement.
+     *
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     */
+    public boolean supportsCatalogsInTableDefinitions() {
+        return supportsCatalogsInTableDefinitions;
+    }
+
+    private boolean supportsCatalogsInIndexDefinitions;
+
+    private void parseSupportsCatalogsInIndexDefinitions() {
+        this.supportsCatalogsInIndexDefinitions = Throwables.ignoreThrowable(logger, false, new ThrowableFunction<Object, Boolean>() {
+            @Override
+            public Boolean doFun(Object o) throws Throwable {
+                return dbMetaData.supportsCatalogsInIndexDefinitions();
+            }
+        }, null);
+    }
+
+    /**
+     * Retrieves whether a catalog name can be used in an index definition statement.
+     *
+     * @return <code>true</code> if so; <code>false</code> otherwise
+     */
+    public boolean supportsCatalogsInIndexDefinitions() {
+        return supportsCatalogsInIndexDefinitions;
     }
 }
