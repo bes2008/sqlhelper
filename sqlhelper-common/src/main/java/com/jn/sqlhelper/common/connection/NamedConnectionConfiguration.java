573aa19805cc8623b8f385b0330625e83877938e
fs1194361820@163.com <fs1194361820@163.com>
2019-11-08 18:30:52 +0800

    + add named connection configuration

--- sqlhelper-common/src/main/java/com/jn/sqlhelper/common/connection/NamedConnectionConfiguration.java
+++ sqlhelper-common/src/main/java/com/jn/sqlhelper/common/connection/NamedConnectionConfiguration.java
@@ -1,7 +1,31 @@
 package com.jn.sqlhelper.common.connection;
 
-public class NamedConnectionConfiguration extends ConnectionConfiguration {
+import com.jn.sqlhelper.langx.configuration.Configuration;
+
+public class NamedConnectionConfiguration extends ConnectionConfiguration implements Configuration {
     private String name;
+
+    public NamedConnectionConfiguration() {
+
+    }
+
+    public NamedConnectionConfiguration(ConnectionConfiguration configuration) {
+        setDriver(configuration.getDriver());
+        setUser(configuration.getUser());
+        setPassword(configuration.getPassword());
+        setUrl(configuration.getUrl());
+        setDriverProps(configuration.getDriverProps());
+    }
+
+    @Override
+    public void setId(String id) {
+        name = id;
+    }
+
+    @Override
+    public String getId() {
+        return name;
+    }
 
     public String getName() {
         return name;
