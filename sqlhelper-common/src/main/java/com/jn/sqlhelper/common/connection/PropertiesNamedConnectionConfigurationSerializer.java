d99b64468192124074279ae2beba9b42afd41984
fs1194361820@163.com <fs1194361820@163.com>
2019-11-11 14:13:52 +0800

    * implement a properties writer

--- sqlhelper-common/src/main/java/com/jn/sqlhelper/common/connection/PropertiesNamedConnectionConfigurationSerializer.java
+++ sqlhelper-common/src/main/java/com/jn/sqlhelper/common/connection/PropertiesNamedConnectionConfigurationSerializer.java
@@ -14,11 +14,41 @@
 
 package com.jn.sqlhelper.common.connection;
 
+import com.jn.langx.text.StringTemplates;
+import com.jn.langx.util.collection.Collects;
+import com.jn.langx.util.function.Consumer2;
 import com.jn.sqlhelper.langx.configuration.ConfigurationSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Properties;
 
 public class PropertiesNamedConnectionConfigurationSerializer implements ConfigurationSerializer<NamedConnectionConfiguration, String> {
+    private static final Logger logger = LoggerFactory.getLogger(PropertiesNamedConnectionConfigurationSerializer.class);
+
     @Override
     public String serialize(NamedConnectionConfiguration configuration) {
-        return null;
+        final Properties props = new Properties();
+        props.setProperty(ConnectionConfiguration.DRIVER, configuration.getDriver());
+        props.setProperty(ConnectionConfiguration.URL, configuration.getUrl());
+        props.setProperty(ConnectionConfiguration.USER, configuration.getUser());
+        props.setProperty(ConnectionConfiguration.PASSWORD, configuration.getPassword());
+        Collects.forEach(configuration.getDriverProps(), new Consumer2<Object, Object>() {
+            @Override
+            public void accept(Object key, Object value) {
+                props.setProperty(key.toString(), value.toString());
+            }
+        });
+
+        StringWriter stringWriter = new StringWriter();
+        String comment = StringTemplates.formatWithPlaceholder("JDBC Connection Configuration [{}]", configuration.getId());
+        try {
+            props.store(stringWriter, comment);
+        } catch (IOException ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+        return stringWriter.toString();
     }
 }
