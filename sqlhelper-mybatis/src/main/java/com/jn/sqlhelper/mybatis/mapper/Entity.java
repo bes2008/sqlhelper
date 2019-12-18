fad8014913a98fbca9e41056da1ea9920d6fc181
fs1194361820@163.com <fs1194361820@163.com>
2019-12-18 16:59:18 +0800

    * supports mapper

--- sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/mapper/Entity.java
+++ sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/mapper/Entity.java
@@ -14,35 +14,6 @@
 
 package com.jn.sqlhelper.mybatis.mapper;
 
-import java.util.List;
-
-public interface BaseMapper<E extends Entity<ID>, ID> {
-    void insert(E entity);
-
-    void update(E entity);
-
-    void delete(E entity);
-
-    void merge(E entity);
-
-    void deleteById(ID id);
-
-    void updateById(ID id);
-
-    E selectById(ID id);
-
-    void deleteByIds(List<ID> ids);
-
-    List<E> selectByIds(List<ID> ids);
-
-    void update(List<E> entities);
-
-    List<E> selectAll();
-
-    Integer selectTotalCount();
-
-    List<E> selectByLimit(E limit);
-
-    Integer selectCountByLimit(E limit);
-
+public interface Entity<ID> {
+    ID getId();
 }
