493ef76fbd21d7d55bf6862390d6f9eae953cc88
fangjinuo <fs1194361820@163.com>
2019-12-21 23:11:16 +0800

    + add like parameter escaper plugin

---
+++ sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/plugins/LikeParameterEscapePlugin.java
@@ -0,0 +1,59 @@
+package com.jn.sqlhelper.mybatis.plugins;
+
+import com.jn.langx.lifecycle.Initializable;
+import com.jn.langx.lifecycle.InitializationException;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+@Intercepts({
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+        @Signature(type = Executor.class, method = "queryCursor", args = {MappedStatement.class, Object.class, RowBounds.class}),
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
+})
+public class LikeParameterEscapePlugin implements Interceptor, Initializable {
+    private static final Logger logger = LoggerFactory.getLogger(LikeParameterEscapePlugin.class);
+
+    @Override
+    public void init() throws InitializationException {
+
+    }
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        if (logger.isDebugEnabled()) {
+            logger.debug("{}", invocation);
+        }
+        final Object[] args = invocation.getArgs();
+        final MappedStatement ms = (MappedStatement) args[0];
+        final Object parameter = args[1];
+        final Executor executor = (Executor) invocation.getTarget();
+
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        if (target instanceof Executor) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("wrap mybatis executor {}", target.getClass());
+            }
+            return Plugin.wrap(target, this);
+        }
+        return target;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
