0217661a06b8f7321c93f9c948db05273c501829
fangjinuo <fs1194361820@163.com>
2020-01-01 22:18:21 +0800

    * find a likeEscaper

--- sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/plugins/likeescape/LikeParameterEscapePlugin.java
+++ sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/plugins/likeescape/LikeParameterEscapePlugin.java
@@ -1,9 +1,11 @@
 package com.jn.sqlhelper.mybatis.plugins.likeescape;
 
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.annotation.Nullable;
 import com.jn.langx.lifecycle.Initializable;
 import com.jn.langx.lifecycle.InitializationException;
-import com.jn.sqlhelper.dialect.SqlRequestContext;
-import com.jn.sqlhelper.dialect.SqlRequestContextHolder;
+import com.jn.langx.util.Strings;
+import com.jn.sqlhelper.dialect.*;
 import com.jn.sqlhelper.mybatis.MybatisUtils;
 import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
@@ -25,6 +27,7 @@ import java.util.Properties;
 })
 public class LikeParameterEscapePlugin implements Interceptor, Initializable {
     private static final Logger logger = LoggerFactory.getLogger(LikeParameterEscapePlugin.class);
+    private static final SQLStatementInstrumentor instrumentor = new SQLStatementInstrumentor();
 
     @Override
     public void init() throws InitializationException {
@@ -41,10 +44,10 @@ public class LikeParameterEscapePlugin implements Interceptor, Initializable {
         if (!MybatisUtils.isPreparedStatement(ms)) {
             return invocation.proceed();
         }
-        SqlRequestContext sqlContext = SqlRequestContextHolder.getInstance().get();
-        if(sqlContext == null || !sqlContext.getRequest().isEscapeLikeParameter()){
+        if (!isEnableLikeEscape()) {
             return invocation.proceed();
         }
+
         final Object parameter = args[1];
         final Executor executor = (Executor) invocation.getTarget();
 
@@ -66,9 +69,34 @@ public class LikeParameterEscapePlugin implements Interceptor, Initializable {
         }
 
         String sql = boundSql.getSql();
-
-
+        SqlRequestContext sqlContext = SqlRequestContextHolder.getInstance().get();
+        LikeEscaper likeEscaper = getLikeEscaper(ms, sqlContext.getRequest());
         return invocation.proceed();
+    }
+
+    private boolean isEnableLikeEscape() {
+        SqlRequestContext sqlContext = SqlRequestContextHolder.getInstance().get();
+        if (sqlContext == null) {
+            // using global configuration
+            return false;
+        } else {
+            SqlRequest sqlRequest = sqlContext.getRequest();
+            return sqlRequest != null && sqlRequest.isEscapeLikeParameter();
+        }
+    }
+
+    private LikeEscaper getLikeEscaper(@NonNull MappedStatement ms, @Nullable SqlRequest sqlRequest) {
+        LikeEscaper likeEscaper = null;
+        if (sqlRequest != null) {
+            likeEscaper = sqlRequest.getLikeEscaper();
+        }
+        if (likeEscaper == null) {
+            String databaseId = MybatisUtils.getDatabaseId(SqlRequestContextHolder.getInstance(), instrumentor, ms);
+            if (Strings.isNotBlank(databaseId)) {
+                likeEscaper = instrumentor.getDialectRegistry().getDialectByName(databaseId);
+            }
+        }
+        return likeEscaper;
     }
 
     @Override
