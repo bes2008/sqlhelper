2791f0ad3a6480f963f46ffdfc6f8fa5d6dd0668
fs1194361820@163.com <fs1194361820@163.com>
2020-01-02 21:51:33 +0800

    * add more fields

--- sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/plugins/ExecutorInvocation.java
+++ sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/plugins/ExecutorInvocation.java
@@ -14,12 +14,70 @@
 
 package com.jn.sqlhelper.mybatis.plugins;
 
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 
 public class ExecutorInvocation {
     private Invocation invocation;
 
-    public ExecutorInvocation(Invocation invocation){
+    private Executor executor;
+    private Object parameter;
+    private MappedStatement mappedStatement;
+    private RowBounds rowBounds;
+    private ResultHandler resultHandler;
+    private CacheKey cacheKey;
+    private BoundSql boundSql;
+
+    public ExecutorInvocation(Invocation invocation) {
         this.invocation = invocation;
+    }
+
+    private void parseQuery() {
+
+    }
+
+    private void parseQueryCursor() {
+
+    }
+
+    private void parseUpdate() {
+
+    }
+
+    public Invocation getInvocation() {
+        return invocation;
+    }
+
+    public Executor getExecutor() {
+        return executor;
+    }
+
+    public Object getParameter() {
+        return parameter;
+    }
+
+    public MappedStatement getMappedStatement() {
+        return mappedStatement;
+    }
+
+    public RowBounds getRowBounds() {
+        return rowBounds;
+    }
+
+    public ResultHandler getResultHandler() {
+        return resultHandler;
+    }
+
+    public CacheKey getCacheKey() {
+        return cacheKey;
+    }
+
+    public BoundSql getBoundSql() {
+        return boundSql;
     }
 }
