dc1a9838ce6df2d80b1f7639e15b2d79bfc83af8
fs1194361820@163.com <fs1194361820@163.com>
2020-01-02 21:41:13 +0800

    * an new plugin

---
+++ sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/plugins/SqlHelperMybatisPlugin.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the LGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jn.sqlhelper.mybatis.plugins;
+
+import com.jn.langx.lifecycle.Initializable;
+import com.jn.langx.lifecycle.InitializationException;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+@Intercepts({
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+        @Signature(type = Executor.class, method = "queryCursor", args = {MappedStatement.class, Object.class, RowBounds.class}),
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
+})
+public class SqlHelperMybatisPlugin implements Interceptor, Initializable {
+    private static final Logger logger = LoggerFactory.getLogger(SqlHelperMybatisPlugin.class);
+
+    @Override
+    public void init() throws InitializationException {
+
+    }
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        return null;
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        if (target instanceof Executor) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("wrap mybatis executor {}", target.getClass());
+            }
+            return Plugin.wrap(target, this);
+        }
+        return target;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
