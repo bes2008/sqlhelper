726b633ab1ecc28c95cdf8eb00fadd80f1e6a5e5
fs1194361820@163.com <fs1194361820@163.com>
2020-01-04 15:23:34 +0800

    + add langx prototype annotation

---
+++ sqlhelper-common/src/main/java/com/jn/langx/annotation/Prototype.java
@@ -0,0 +1,12 @@
+package com.jn.langx.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
+public @interface Prototype {
+}
