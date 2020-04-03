bd9029054897dcd93969d631d09d169b82532527
fangjinuo <fs1194361820@163.com>
2020-04-03 23:20:27 +0800

    * supports some expression

---
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/ast/expression/DoubleExpression.java
@@ -0,0 +1,14 @@
+package com.jn.sqlhelper.dialect.ast.expression;
+
+import com.jn.langx.expression.value.NumberExpression;
+
+public class DoubleExpression extends NumberExpression<Double> implements SQLExpression<Double> {
+    public DoubleExpression(Double number) {
+        super(number);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
