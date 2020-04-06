e8e33793a7eadf71343edc8c567d43222ad3479c
fs1194361820@163.com <fs1194361820@163.com>
2020-04-06 18:22:50 +0800

    * supports expression converters: int, long

---
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/sqlparser/jsqlparser/expression/IntegerOrLongValueExpressionConverter.java
@@ -0,0 +1,26 @@
+package com.jn.sqlhelper.dialect.sqlparser.jsqlparser.expression;
+
+import com.jn.sqlhelper.dialect.expression.IntegerOrLongExpression;
+import net.sf.jsqlparser.expression.LongValue;
+
+public class IntegerOrLongValueExpressionConverter implements ExpressionConverter<IntegerOrLongExpression, LongValue> {
+    @Override
+    public LongValue toJSqlParserExpression(IntegerOrLongExpression expression) {
+        return new LongValue(expression.getValue());
+    }
+
+    @Override
+    public IntegerOrLongExpression fromJSqlParserExpression(LongValue expression) {
+        return null;
+    }
+
+    @Override
+    public Class<IntegerOrLongExpression> getStandardExpressionClass() {
+        return IntegerOrLongExpression.class;
+    }
+
+    @Override
+    public Class<LongValue> getJSqlParserExpressionClass() {
+        return LongValue.class;
+    }
+}
