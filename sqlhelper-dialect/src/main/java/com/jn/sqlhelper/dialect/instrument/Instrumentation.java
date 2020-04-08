0e8f4c9de0706f6e8b503ba066236ca787435677
fs1194361820@163.com <fs1194361820@163.com>
2020-04-08 16:23:45 +0800

    * refactor code

--- sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/instrument/Instrumentation.java
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/instrument/Instrumentation.java
@@ -1,14 +1,17 @@
 package com.jn.sqlhelper.dialect.instrument;
 
+import com.jn.langx.lifecycle.Initializable;
 import com.jn.sqlhelper.dialect.instrument.orderby.OrderByTransformer;
 import com.jn.sqlhelper.dialect.instrument.where.WhereTransformer;
 import com.jn.sqlhelper.dialect.sqlparser.SqlParser;
 import com.jn.sqlhelper.dialect.sqlparser.SqlStatementWrapper;
 
-public interface Instrumentation<Statement, SQL extends SqlStatementWrapper<Statement>> {
+public interface Instrumentation<Statement, SQL extends SqlStatementWrapper<Statement>> extends Initializable {
     SqlParser<SQL> getSqlParser();
 
     WhereTransformer<Statement> getWhereTransformer();
 
     OrderByTransformer<Statement> getOrderByTransformer();
+
+    boolean isEnabled();
 }
--- sqlhelper-jsqlparser/src/main/java/com/jn/sqlhelper/jsqlparser/instrument/JSqlParserInstrumentation.java
+++ sqlhelper-jsqlparser/src/main/java/com/jn/sqlhelper/jsqlparser/instrument/JSqlParserInstrumentation.java
@@ -1,5 +1,6 @@
 package com.jn.sqlhelper.jsqlparser.instrument;
 
+import com.jn.langx.lifecycle.InitializationException;
 import com.jn.sqlhelper.dialect.instrument.Instrumentation;
 import com.jn.sqlhelper.dialect.instrument.orderby.OrderByTransformer;
 import com.jn.sqlhelper.dialect.instrument.where.WhereTransformer;
@@ -8,18 +9,34 @@ import com.jn.sqlhelper.jsqlparser.sqlparser.JSqlParserStatementWrapper;
 import net.sf.jsqlparser.statement.Statement;
 
 public class JSqlParserInstrumentation implements Instrumentation<Statement, JSqlParserStatementWrapper> {
+    private boolean enabled = false;
+    private SqlParser<JSqlParserStatementWrapper> sqlParser;
+    private WhereTransformer<Statement> whereTransformer;
+    private OrderByTransformer<Statement> orderByTransformer;
+
+    @Override
+    public void init() throws InitializationException {
+
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
     @Override
     public SqlParser<JSqlParserStatementWrapper> getSqlParser() {
-        return null;
+        return this.sqlParser;
     }
 
     @Override
     public WhereTransformer<Statement> getWhereTransformer() {
-        return null;
+        return this.whereTransformer;
     }
 
     @Override
     public OrderByTransformer<Statement> getOrderByTransformer() {
-        return null;
+        return this.orderByTransformer;
     }
+
 }
