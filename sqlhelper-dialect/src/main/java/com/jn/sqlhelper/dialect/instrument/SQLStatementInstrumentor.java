c059920ac3c28cff900cb9fdb1d0e5f278eefaf3
fs1194361820@163.com <fs1194361820@163.com>
2020-04-08 18:25:19 +0800

    * initial the specified instrumentation

--- sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/instrument/SQLStatementInstrumentor.java
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/instrument/SQLStatementInstrumentor.java
@@ -107,8 +107,9 @@ public class SQLStatementInstrumentor {
                         })
                         .build();
             }
-            this.instrumentation = InstrumentationRegistry.getInstance().findInstrumentation();
-            Preconditions.checkNotNull(instrumentation,"Can't find a suitable SQL instrumentation");
+            InstrumentationRegistry.getInstance().enableInstrumentation(this.config.getInstrumentation());
+            this.instrumentation = InstrumentationRegistry.getInstance().findInstrumentation(this.config.getInstrumentation());
+            Preconditions.checkNotNull(instrumentation,"Can't find a suitable or enabled SQL instrumentation");
             orderByTransformer = new DefaultOrderByTransformer();
             orderByTransformer.setInstrumentation(instrumentation);
             orderByTransformer.init();
