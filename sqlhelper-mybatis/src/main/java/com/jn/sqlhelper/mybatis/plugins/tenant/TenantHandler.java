6ed4c0eb3d3d700240ad5027c8eb678d3fc6cec0
smallgray <huxiongming@outlook.com>
2020-04-26 20:30:13 +0800

    # 增加租户模式

--- sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/plugins/tenant/TenantHandler.java
+++ sqlhelper-mybatis/src/main/java/com/jn/sqlhelper/mybatis/plugins/tenant/TenantHandler.java
@@ -16,18 +16,20 @@ import com.jn.sqlhelper.mybatis.plugins.ExecutorInvocation;
 import com.jn.sqlhelper.mybatis.plugins.SqlHelperMybatisPlugin;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.statement.StatementHandler;
-import org.apache.ibatis.mapping.BoundSql;
-import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.mapping.*;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -35,6 +37,8 @@ import java.util.Properties;
  */
 public class TenantHandler extends AbstractHandler{
     private static Logger logger = LoggerFactory.getLogger(TenantHandler.class);
+    private static final String TENANT_SUFFIX = "_tenant";
+
     @Override
     public void inbound(HandlerContext ctx) throws Throwable {
         SqlRequestContext sqlContext = SqlRequestContextHolder.getInstance().get();
@@ -54,23 +58,62 @@ public class TenantHandler extends AbstractHandler{
         ExecutorInvocation executorInvocation = (ExecutorInvocation) ctx.getPipeline().getTarget();
         MappedStatement ms = executorInvocation.getMappedStatement();
         BoundSql boundSql = executorInvocation.getBoundSql();
+        final Executor executor = executorInvocation.getExecutor();
         final Tenant tenant=sqlContext.getRequest().getTenant();
+        final Object parameter = executorInvocation.getParameter();
+        final String tenantStatementId = this.getTenantStatementId(ms, tenant);
         try {
-            String newSql=getTenantSql(boundSql,tenant);
-            boundSql = MybatisUtils.rebuildBoundSql(newSql, ms.getConfiguration(), boundSql);
+            SQLStatementInstrumentor instrumentor = SqlHelperMybatisPlugin.getInstrumentor();
+           // String tenantSql = instrumentor.instrumentTenantSql(boundSql.getSql(), tenant);
+           String tenantSql="UPDATE USER SET NAME = ?, AGE = ? WHERE TENANTID = ? AND ID = ? ";
+            MappedStatement tenantStatement = this.customTenantStatement(ms, tenantStatementId);
+            boundSql = MybatisUtils.rebuildBoundSql(tenantSql, tenantStatement.getConfiguration(), boundSql);
+            executor.update(tenantStatement,parameter);
             executorInvocation.setBoundSql(boundSql);
             Pipelines.inbound(ctx);
         }catch (Throwable e){
             e.printStackTrace();
         }
-
     }
 
+    private String getTenantStatementId(final MappedStatement ms, final Tenant tenant) {
+        String tenantString = tenant.toString();
+        StringBuilder builder = new StringBuilder(ms.getId() + "_");
+        for (int i = 0; i < tenantString.length(); i++) {
+            char c = tenantString.charAt(i);
+            // 0-9, a-z, _
+            if (Chars.isNumber(c) || Chars.isLowerCase(c) || Chars.isUpperCase(c) || c == '_') {
+                builder.append(c);
+            }
+        }
+        return builder.append(TENANT_SUFFIX).toString();
+    }
 
-    private String getTenantSql(BoundSql boundSql,Tenant tenant) {
-        SQLStatementInstrumentor instrumentor = SqlHelperMybatisPlugin.getInstrumentor();
-        final String tenantSql = instrumentor.instrumentTenantSql(boundSql.getSql(), tenant);
-        return tenantSql;
+    private MappedStatement customTenantStatement(final MappedStatement ms, final String tenantStatementId) {
+        final MappedStatement.Builder builder = new MappedStatement.Builder(ms.getConfiguration(), tenantStatementId, ms.getSqlSource(), ms.getSqlCommandType());
+        builder.resource(ms.getResource());
+        builder.fetchSize(ms.getFetchSize());
+        builder.statementType(ms.getStatementType());
+        builder.keyGenerator(ms.getKeyGenerator());
+        if (Emptys.isNotEmpty(ms.getKeyProperties())) {
+            final StringBuilder keyProperties = new StringBuilder();
+            for (final String keyProperty : ms.getKeyProperties()) {
+                keyProperties.append(keyProperty).append(",");
+            }
+            keyProperties.delete(keyProperties.length() - 1, keyProperties.length());
+            builder.keyProperty(keyProperties.toString());
+        }
+        final List<ResultMap> resultMaps = new ArrayList<ResultMap>();
+        final ResultMap resultMap = new ResultMap.Builder(ms.getConfiguration(), ms.getId(), Long.class, new ArrayList<ResultMapping>()).build();
+        resultMaps.add(resultMap);
+        builder.resultMaps(resultMaps);
+        builder.timeout(ms.getTimeout());
+        builder.parameterMap(ms.getParameterMap());
+        builder.resultSetType(ms.getResultSetType());
+        builder.cache(ms.getCache());
+        builder.flushCacheRequired(ms.isFlushCacheRequired());
+        builder.useCache(ms.isUseCache());
+        return builder.build();
     }
 
 }
