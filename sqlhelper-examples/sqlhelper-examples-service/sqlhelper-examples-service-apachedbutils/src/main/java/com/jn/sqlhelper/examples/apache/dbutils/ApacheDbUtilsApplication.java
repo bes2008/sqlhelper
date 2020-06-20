2bc4ae5357b67b166e8cce090495e26efaf36b9a
fs1194361820@163.com <fs1194361820@163.com>
2020-06-21 00:26:40 +0800

    * 整理 Swagger 依赖

--- sqlhelper-examples/sqlhelper-examples-service/sqlhelper-examples-service-apachedbutils/src/main/java/com/jn/sqlhelper/examples/apache/dbutils/ApacheDbUtilsApplication.java
+++ sqlhelper-examples/sqlhelper-examples-service/sqlhelper-examples-service-apachedbutils/src/main/java/com/jn/sqlhelper/examples/apache/dbutils/ApacheDbUtilsApplication.java
@@ -3,8 +3,10 @@ package com.jn.sqlhelper.examples.apache.dbutils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.jn.sqlhelper.examples.apache.dbutils",
-        "com.jn.sqlhelper.examples.db.config"
+@SpringBootApplication(scanBasePackages = {
+        "com.jn.sqlhelper.examples.apache.dbutils",
+        "com.jn.sqlhelper.examples.db.config",
+        "com.jn.sqlhelper.examples.swagger.config"
 })
 public class ApacheDbUtilsApplication {
     public static void main(String[] args) {
---
+++ sqlhelper-examples/sqlhelper-examples-swagger/src/main/java/com/jn/sqlhelper/examples/swagger/config/Swagger2Config.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the LGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jn.sqlhelper.examples.swagger.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableConfigurationProperties(SwaggerProperties.class)
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi(SwaggerProperties swaggerProperties ) {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo(swaggerProperties))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(SwaggerProperties swaggerProperties ) {
+        return new ApiInfoBuilder()
+                .title(swaggerProperties.getApplicationTitle())
+                .description("https://github.com/fangjinuo  http://www.cnblogs.com/f1194361820")
+                .termsOfServiceUrl("https://github.com/fangjinuo")
+                .contact("fs1194361820@163.com")
+                .version("2.0")
+                .build();
+
+    }
+}
