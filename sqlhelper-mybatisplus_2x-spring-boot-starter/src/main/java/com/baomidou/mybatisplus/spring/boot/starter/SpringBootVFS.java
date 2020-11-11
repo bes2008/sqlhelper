63533da20ee2916897069e2618f4ac1169661eef
fjn <1194361820@qq.com>
2020-11-11 10:25:18 +0800

    * 迁移mybatis-plus 2.x spring boot starter到sqlhelper

---
+++ sqlhelper-mybatisplus_2x-spring-boot-starter/src/main/java/com/baomidou/mybatisplus/spring/boot/starter/SpringBootVFS.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the LGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.baomidou.mybatisplus.spring.boot.starter;
+
+import org.apache.ibatis.io.VFS;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Hans Westerbeek
+ * @author Eddú Meléndez
+ * @author Kazuki Shimizu
+ */
+public class SpringBootVFS extends VFS {
+
+    private final ResourcePatternResolver resourceResolver;
+
+    public SpringBootVFS() {
+        this.resourceResolver = new PathMatchingResourcePatternResolver(getClass().getClassLoader());
+    }
+
+    private static String preserveSubpackageName(final URI uri, final String rootPath) {
+        final String uriStr = uri.toString();
+        final int start = uriStr.indexOf(rootPath);
+        return uriStr.substring(start);
+    }
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+    @Override
+    protected List<String> list(URL url, String path) throws IOException {
+        Resource[] resources = resourceResolver.getResources("classpath*:" + path + "/**/*.class");
+        List<String> resourcePaths = new ArrayList<String>();
+        for (Resource resource : resources) {
+            resourcePaths.add(preserveSubpackageName(resource.getURI(), path));
+        }
+        return resourcePaths;
+    }
+
+}
