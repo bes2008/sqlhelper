758dce54a42d51ce37d6941a79238e663b84a8b8
fjn <1194361820@qq.com>
2020-12-19 15:22:10 +0800

    * 重构代码

--- sqlhelper-datasource/src/main/java/com/jn/sqlhelper/datasource/DataSource.java
+++
@@ -1,7 +0,0 @@
-package com.jn.sqlhelper.datasource;
-
-public @interface DataSource {
-    public String group() default DataSources.DATASOURCE_GROUP_DEFAULT;
-
-    public String name() default "";
-}
