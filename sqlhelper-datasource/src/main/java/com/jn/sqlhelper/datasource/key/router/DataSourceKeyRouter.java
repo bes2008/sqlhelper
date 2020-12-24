de87272bb701bb232e753fe77a4a390eaae02ae4
fjn <1194361820@qq.com>
2020-12-24 18:02:50 +0800

    * 代码整理

--- sqlhelper-datasource/src/main/java/com/jn/sqlhelper/datasource/key/router/DataSourceKeyRouter.java
+++ sqlhelper-datasource/src/main/java/com/jn/sqlhelper/datasource/key/router/DataSourceKeyRouter.java
@@ -26,7 +26,7 @@ import java.util.List;
 public interface DataSourceKeyRouter extends Function2<List<DataSourceKey>, MethodInvocation, DataSourceKey>, Named {
     /**
      * @param dataSourceKeys 该参数为经过筛选后的 key，筛选过程可以参见 {@link com.jn.sqlhelper.datasource.key.DataSourceKeySelector#select(AbstractDataSourceKeyRouter, MethodInvocation)}
-     *                       如果开起了故障转移，则在筛选时，会自动的移除掉故障的。
+     *                       如果开启了故障转移，则在筛选时，会自动的移除掉故障的。
      * @return 返回选中的datasource key, 如果没有合适的，返回 null 即可.
      */
     @Override
