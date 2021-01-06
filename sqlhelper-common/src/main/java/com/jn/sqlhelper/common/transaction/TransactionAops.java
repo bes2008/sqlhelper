9f85198e1e981815c78e53ea8375650adb170212
fjn <1194361820@qq.com>
2021-01-06 09:41:20 +0800

    * 把事务处理的逻辑提取出来

---
+++ sqlhelper-common/src/main/java/com/jn/sqlhelper/common/transaction/TransactionAops.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at  http://www.gnu.org/licenses/lgpl-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jn.sqlhelper.common.transaction;
+
+import com.jn.langx.annotation.NonNull;
+import com.jn.langx.invocation.MethodInvocation;
+import com.jn.langx.util.Preconditions;
+import com.jn.sqlhelper.common.transaction.definition.TransactionDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TransactionAops {
+    private static final Logger logger = LoggerFactory.getLogger(TransactionAops.class);
+
+    public static Object invoke(@NonNull TransactionManager transactionManager, @NonNull TransactionDefinition definition, @NonNull MethodInvocation invocation) throws Throwable {
+        Preconditions.checkNotNull(transactionManager, "the transaction manager is null");
+        Preconditions.checkNotNull(definition, "the transaction definition is null");
+
+        Transaction transaction = Transactions.get();
+
+        // 是否嵌入在一个已有事务内部
+        boolean nested = transaction != null;
+
+        // 定义了事务
+        if (!nested) {
+            transaction = transactionManager.createTransaction(definition);
+        }
+        Transactions.bind(transaction);
+
+        try {
+            Object ret = invocation.proceed();
+            // 接下来是要提交事务了
+
+            // 但是发现了内层事务被回滚了。
+            if (!nested) {
+                if (transaction.isRollbackOnly()) {
+                    // do log
+                    transactionManager.rollback(transaction);
+                } else {
+                    transactionManager.commit(transaction);
+                }
+            }
+            return ret;
+        } catch (Throwable ex) {
+            boolean rollback = transaction.isRollbackOnly();
+            if (!rollback) {
+                rollback = Transactions.willRollback(ex, definition);
+            }
+            if (!rollback) {
+                if (nested) {
+                    // log it
+                } else {
+                    transactionManager.commit(transaction);
+                }
+            } else {
+                if (nested) {
+                    // 标记内层事务被回滚了
+                    transaction.setRollbackOnly();
+                }
+                transactionManager.rollback(transaction);
+            }
+            return ex;
+        } finally {
+            if (!nested) {
+                transaction.clearResources();
+                Transactions.unbind();
+            }
+        }
+    }
+
+}
--- sqlhelper-datasource/src/main/java/com/jn/sqlhelper/datasource/spring/aop/LocalizeGlobalTransactionInterceptor.java
+++ sqlhelper-datasource/src/main/java/com/jn/sqlhelper/datasource/spring/aop/LocalizeGlobalTransactionInterceptor.java
@@ -1,16 +1,17 @@
 package com.jn.sqlhelper.datasource.spring.aop;
 
-import com.jn.langx.annotation.Nullable;
-import com.jn.sqlhelper.common.transaction.Transaction;
+import com.jn.agileway.aop.adapter.aopalliance.MethodInvocationAdapter;
+import com.jn.sqlhelper.common.transaction.TransactionAops;
 import com.jn.sqlhelper.common.transaction.TransactionManager;
-import com.jn.sqlhelper.common.transaction.Transactions;
 import com.jn.sqlhelper.common.transaction.definition.TransactionDefinition;
 import com.jn.sqlhelper.common.transaction.definition.TransactionDefinitionRegistry;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LocalizeGlobalTransactionInterceptor implements MethodInterceptor {
-
+    private static final Logger logger = LoggerFactory.getLogger(LocalizeGlobalTransactionInterceptor.class);
     private TransactionManager transactionManager;
     private TransactionDefinitionRegistry definitionRegistry;
 
@@ -24,61 +25,16 @@ public class LocalizeGlobalTransactionInterceptor implements MethodInterceptor {
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
-        @Nullable
+        if (transactionManager == null || definitionRegistry == null) {
+            return invocation.proceed();
+        }
+
         TransactionDefinition definition = definitionRegistry.get(invocation.getMethod());
-        // 当前方法没有指定事务
         if (definition == null) {
             return invocation.proceed();
         }
 
-        Transaction transaction = Transactions.get();
-
-        // 是否嵌入在一个已有事务内部
-        boolean nested = transaction != null;
-
-        // 定义了事务
-        if (!nested) {
-            transaction = transactionManager.createTransaction(definition);
-        }
-        Transactions.bind(transaction);
-
-        try {
-            Object ret = invocation.proceed();
-            // 接下来是要提交事务了
-
-            // 但是发现了内层事务被回滚了。
-            if (!nested) {
-                if (transaction.isRollbackOnly()) {
-                    transactionManager.rollback(transaction);
-                } else {
-                    transactionManager.commit(transaction);
-                }
-            }
-            return ret;
-        } catch (Throwable ex) {
-            boolean rollback = transaction.isRollbackOnly();
-            if (!rollback) {
-                rollback = Transactions.willRollback(ex, definition);
-            }
-            if (!rollback) {
-                if (nested) {
-                    // log it
-                } else {
-                    transactionManager.commit(transaction);
-                }
-            } else {
-                if (nested) {
-                    // 标记内层事务被回滚了
-                    transaction.setRollbackOnly();
-                }
-                transactionManager.rollback(transaction);
-            }
-            return ex;
-        } finally {
-            if (!nested) {
-                transaction.clearResources();
-                Transactions.unbind();
-            }
-        }
+        MethodInvocationAdapter adapter = new MethodInvocationAdapter(invocation);
+        return TransactionAops.invoke(transactionManager, definition, adapter);
     }
 }
