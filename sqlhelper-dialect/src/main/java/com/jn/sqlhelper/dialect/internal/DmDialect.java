d6df99e186634944cbd2411f72e2525a152229e1
fjn1194361820 <fs1194361820@163.com>
2024-09-10 15:36:05 +0800

    * 代码整理

--- sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/internal/DmDialect.java
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/internal/DmDialect.java
@@ -16,6 +16,9 @@ package com.jn.sqlhelper.dialect.internal;
 
 import com.jn.sqlhelper.dialect.internal.limit.LimitOffsetLimitHandler;
 
+/**
+ * 在使用 DM数据库时，通常要关闭大小写敏感
+ */
 public class DmDialect extends AbstractDialect {
     public DmDialect() {
         setLimitHandler(new LimitOffsetLimitHandler());
@@ -31,4 +34,8 @@ public class DmDialect extends AbstractDialect {
         return true;
     }
 
+    @Override
+    public IdentifierCase unquotedIdentifierCase() {
+        return IdentifierCase.IGNORE_CASE;
+    }
 }
