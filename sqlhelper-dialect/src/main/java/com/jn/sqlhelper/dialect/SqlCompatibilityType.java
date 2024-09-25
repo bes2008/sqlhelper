631b354edc7f334f63b4ece42bf114c244823722
fjn1194361820 <fs1194361820@163.com>
2024-09-25 16:53:35 +0800

    * 提供数据库兼容性支持

--- sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/SqlCompatibilityType.java
+++ sqlhelper-dialect/src/main/java/com/jn/sqlhelper/dialect/SqlCompatibilityType.java
@@ -1,13 +1,36 @@
 package com.jn.sqlhelper.dialect;
 
-public enum SqlCompatibilityType {
-    ORACLE,
-    MYSQL,
-    TERADATA,
-    POSTGRESQL,
-    SQLSERVER,
-    SQL92,
+import com.jn.langx.util.enums.base.CommonEnum;
+import com.jn.langx.util.enums.base.EnumDelegate;
+
+public enum SqlCompatibilityType implements CommonEnum {
+    ORACLE(1,"oracle","oracle"),
+    MYSQL(2,"mysql","mysql"),
+    TERADATA(3,"teradata","teradata"),
+    POSTGRESQL(4,"postgresql","postgresql"),
+    SQLSERVER(5,"sqlserver","sqlserver"),
+    SQL92(12,"sql92","sql92"),
 
 
-    NON_COMPATIBILITY
+    NON_COMPATIBILITY(100,"non_compatibility","non_compatibility");
+
+    private EnumDelegate delegate;
+    SqlCompatibilityType(int code, String name ,String displayText){
+        this.delegate = new EnumDelegate(code, name, displayText);
+    }
+
+    @Override
+    public int getCode() {
+        return this.delegate.getCode();
+    }
+
+    @Override
+    public String getDisplayText() {
+        return this.delegate.getDisplayText();
+    }
+
+    @Override
+    public String getName() {
+        return this.delegate.getName();
+    }
 }
